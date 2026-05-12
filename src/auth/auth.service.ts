17516c53e0d40f990c0774542f57c0df6d101e4b
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-05-12 14:17:34 -0300

    aula 232

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return 'Olá do Auth service!';
+  login(loginDto: LoginDto) {
+    //console.log(body);
+    //return 'Olá do Auth service!';
+    return loginDto;
   }
 }
