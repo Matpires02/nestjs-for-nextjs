86fe33986e302eed8fd468e317cccb3b451ad8e8
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-06-22 16:01:29 -0300

    Aula 259 - Cors e helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { parseCorsWhitelist } from './common/utils/parse-cors-whitelist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,25 @@ async function bootstrap() {
       crossOriginResourcePolicy: { policy: 'cross-origin' },
     }),
   );
+
+  const corsWhiteList = parseCorsWhitelist(process.env.CORS_WHITELIST ?? '');
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined, // Isso é do navegador e para proteger o cliente
+      callback: (...args: any[]) => void,
+    ) => {
+      // Requisição sem origin ou que inclui uma origem conhecida
+      // por corsWhiteList é permitida
+      if (!origin || corsWhiteList.includes(origin)) {
+        return callback(null, true); // Permitido
+      }
+
+      // Requisições com origin mas que não conhecemos
+      // negamos.
+      return callback(new Error('Not allowed by CORS'), false);
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
